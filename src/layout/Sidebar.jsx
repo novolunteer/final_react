8b2af7384ce8161edb89d27eee5f40dbe8d76318
novolunteeer <novolunteeer@gmail.com>
2026-03-28 14:03:30 +0900

    로그인 페이지 수정

--- src/layout/Sidebar.jsx
+++ src/layout/Sidebar.jsx
@@ -20,7 +20,6 @@ const Sidebar = () => {
     { path: "/statistics", label:"통계"},
     { path: "/communication", label:"커뮤니케이션"},
     { path: "/notification", label:"알림"},
-    { path: "/login", label: "로그인" },
     { path: "/chat", label: "채팅" }
   ];
 
